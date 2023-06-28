a2b9c79468cd5c328385ee06a48246334975fe48
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2023-06-28 14:16:04 -0600

    Initial commit

---
+++ Backend/main.py
@@ -0,0 +1,20 @@
+import re
+from datetime import datetime
+import os
+from flask import Flask, make_response, request
+from uuid import uuid5 as idtool
+
+app = Flask(__name__)
+
+
+@app.route("/upload", method='POST')
+def upload_file():
+    
+    response = make_response("<h1>Success</h1>")
+    response.status_code = 200
+    return response
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, passthrough_errors=True)
