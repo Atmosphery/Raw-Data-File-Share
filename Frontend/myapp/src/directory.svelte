ac3089919dfa7df2043be37ee3f4625686d9eb9b
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2023-06-30 11:12:19 -0600

    file and drectory svelte object

---
+++ Frontend/myapp/src/directory.svelte
@@ -0,0 +1,6 @@
+<script>
+	import File from "./file.svelte";
+	export let answer;
+</script>
+
+<p>The answer is {answer}</p>
---
+++ Frontend/myapp/src/file.svelte
@@ -0,0 +1,6 @@
+<script>
+	export let file_name;
+	export let date_modified;
+</script>
+
+<p>The answer is {file_name}</p>
